89a8d8549da2edcc18998e9fc947c178f7b66b44
Micrah <mic_24@live.com>
2019-10-20 06:30:17 +1100

    update module,  fix crash, and finish WIP thanks to Poszer's help

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 ## Dynamic XP
 - Latest build status with azerothcore: [![Build Status](https://travis-ci.org/milestorme/mod-dynamic-xp.svg?branch=master)](https://travis-ci.com/milestorme/mod-dynamic-xp)
+
+# Description
+- Set xp per level range e.g in dynamicxp.conf
+   `Dynamic.XP.Rate.1-9 = 1
+   Dynamic.XP.Rate.10-19 = 2
+   Dynamic.XP.Rate.20-29 = 3
+   Dynamic.XP.Rate.30-39 = 4
+   Dynamic.XP.Rate.40-49 = 5
+   Dynamic.XP.Rate.50-59 = 6
+   Dynamic.XP.Rate.60-69 = 7`
+
 ### Credits
-- Micrah/Milestorme: Module Creator/ Script Update (https://github.com/milestorme).
-- Conanhun Script (https://github.com/conan513).
-- Poszer Script Support (https://github.com/poszer)
+- Micrah/Milestorme: Module Creator / Script Rework (https://github.com/milestorme).
+- Poszer Script Support (https://github.com/poszer) 
+- Conan513 Original Script from AshmaneCore (https://github.com/conan513).
+
+
+
--- src/dynamicxp.cpp
+++ src/dynamicxp.cpp
@@ -1,13 +1,15 @@
 /*
 Credits
+Script reworked by Micrah/Milestorme and Poszer (Poszer is the Best)
 Module Created by Micrah/Milestorme
-Script from https://github.com/conan513 Single Player Project 
+Original Script from AshmaneCore https://github.com/conan513 Single Player Project 
 */
 #include "Configuration/Config.h"
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Chat.h"
 
+float amount;
 
 class spp_dynamic_xp_rate : public PlayerScript
 {
@@ -21,34 +23,34 @@ class spp_dynamic_xp_rate : public PlayerScript
                ChatHandler(player->GetSession()).SendSysMessage("This server is running the |cff4CFF00Level Dynamic XP |rmodule.");
                }
        }
-        void OnGiveXP(Player* player, uint32& /*amount*/, Unit* /*victim*/) override
+        void OnGiveXP(Player* player, uint32& amount, Unit* /*victim*/) override
        {
         if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate", true))
         
 	   {
-            if (player->getLevel() >= 1)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.1-9", 1), 0);
+           if (player->getLevel() >= 1)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.1-9", 10.0f);
 
-            if (player->getLevel() >= 10)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.10-19", 2), 0);
+           if (player->getLevel() >= 10)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.10-19", 20.0f);
 
-            if (player->getLevel() >= 20)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.20-29", 3), 0);
+           if (player->getLevel() >= 20)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.20-29", 30.0f);
 
-            if (player->getLevel() >= 30)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.30-39", 4), 0);
+           if (player->getLevel() >= 30)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.30-39", 40.0f);
 
-            if (player->getLevel() >= 40)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.40-49", 5), 0);
+           if (player->getLevel() >= 40)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.40-49", 50.0f);
 
-            if (player->getLevel() >= 50)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.50-59", 6), 0);
+           if (player->getLevel() >= 50)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.50-59", 60.0f);
+           
+           if (player->getLevel() >= 60)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.60-69", 70.0f);
 
-            if (player->getLevel() >= 60)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.60-69", 7), 0);
-
-            if (player->getLevel() >= 70)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.70-79", 8), 0);
+           if (player->getLevel() >= 70)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.70-79", 80.0f);
         }
 
      }
@@ -56,31 +58,32 @@ class spp_dynamic_xp_rate : public PlayerScript
     {
         if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate", true))
         {
-            if (player->getLevel() >= 1)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.1-9", 1), 0);
+           if (player->getLevel() >= 1)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.1-9", 10.0f);
 
-            if (player->getLevel() >= 10)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.10-19", 2), 0);
+           if (player->getLevel() >= 10)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.10-19", 20.0f);
 
-            if (player->getLevel() >= 20)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.20-29", 3), 0);
+           if (player->getLevel() >= 20)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.20-29", 30.0f);
 
-            if (player->getLevel() >= 30)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.30-39", 4), 0);
+           if (player->getLevel() >= 30)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.30-39", 40.0f);
 
-            if (player->getLevel() >= 40)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.40-49", 5), 0);
+           if (player->getLevel() >= 40)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.40-49", 50.0f);
 
-            if (player->getLevel() >= 50)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.50-59", 6), 0);
+           if (player->getLevel() >= 50)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.50-59", 60.0f);
+           
+           if (player->getLevel() >= 60)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.60-69", 70.0f);
 
-            if (player->getLevel() >= 60)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.60-69", 7), 0);
-
-            if (player->getLevel() >= 70)
-                player->GiveXP(sConfigMgr->GetIntDefault("Dynamic.XP.Rate.70-79", 8), 0);
+           if (player->getLevel() >= 70)
+            amount *= sConfigMgr->GetFloatDefault("Dynamic.XP.Rate.70-79", 80.0f);
 
         }
+           
     }
 };
 
