e32d70a2c2c5b165e02154eed87d798d2c84f219
Micrah <44911744+milestorme@users.noreply.github.com>
2019-10-20 21:13:50 +1100

    update to less than or equal too

--- src/dynamicxp.cpp
+++ src/dynamicxp.cpp
@@ -27,28 +27,28 @@ class spp_dynamic_xp_rate : public PlayerScript
         if (sConfigMgr->GetBoolDefault("Dynamic.XP.Rate", true))
         
 	   {
-           if (player->getLevel() >= 1)
+           if (player->getLevel() <= 1)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.1-9", 1);
 
-           else if (player->getLevel() >= 10)
+           else if (player->getLevel() <= 10)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.10-19", 2);
 
-           else if (player->getLevel() >= 20)
+           else if (player->getLevel() <= 20)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.20-29", 3);
 
-           else if (player->getLevel() >= 30)
+           else if (player->getLevel() <= 30)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.30-39", 4);
 
-           else if (player->getLevel() >= 40)
+           else if (player->getLevel() <= 40)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.40-49", 5);
 
-           else if (player->getLevel() >= 50)
+           else if (player->getLevel() <= 50)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.50-59", 6);
 
-           else if (player->getLevel() >= 60)
+           else if (player->getLevel() <= 60)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.60-69", 7);
 
-           else if (player->getLevel() >= 70)
+           else if (player->getLevel() <= 70)
             amount *= sConfigMgr->GetIntDefault("Dynamic.XP.Rate.70-79", 8);
 
         }
